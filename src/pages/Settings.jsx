7a0cb936320fcb5e333a388845695a326c5482d1
Pαblo Kuhn <p4blo.kuhn@gmail.com>
2023-09-02 03:13:49 -0300

    updated ui settings

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -67,11 +67,22 @@ transition: 1s;
   }
 `;
 
+const OptionWrapper = styled.div`
+display: flex;
+align-items: start;
+justify-content: center;
+flex-direction: column;
+gap: 1em;
+`
+
 export default function Settings() {
   return (
     <>
       <Wrapper>
         <MainTitle text={"SETTINGS"} />
+
+<OptionWrapper>
+
         <OptionCtn>
           <Checkbox checked="cheked" id="Option1" type="checkbox" />
           <Label htmlFor="Option1">Palabras que contengan</Label>
@@ -80,6 +91,9 @@ export default function Settings() {
           <Checkbox id="Option2" type="checkbox" />
           <Label htmlFor="Option2">Sonidos</Label>
         </OptionCtn>
+
+</OptionWrapper>
+
         <HomeButton />
       </Wrapper>
     </>
